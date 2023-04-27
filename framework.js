5a6c18ae91b1237d8a9b6250685e0ecaa6c31252
Raul Zepeda <88167761+raulzpda@users.noreply.github.com>
2023-04-26 18:31:19 -0600

    Update framework.js

--- framework.js
+++ framework.js
@@ -1,1 +1,16 @@
-<>
+window.Framework = {
+    config: {
+        name: "minsait",
+        clientIds: { "mypurecloud.com": "459745a4-e2cb-41e7-b383-1bdb31c95cc9"},
+        settings: {},
+        helpLinks: {},
+        customInteractionAttributes: [],
+      getUserLanguage: function (callback) {
+        callback("es-US");
+    },
+    initialSetup: function (),
+    screenPop: function (searchString, interaction),
+    processCallLog: function (callLog, interaction, eventName, onSuccess, onFailure),
+    openCallLog: function (callLog),
+    contactSearch: function (searchValue, onSuccess, onFailure)
+}
